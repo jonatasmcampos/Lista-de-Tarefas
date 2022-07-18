38077501cf0bf433e15345b63e7d359bd36d2aca
jonatasmcampos <camposjonatas3@gmail.com>
2022-07-18 17:54:22 -0300

    recyclerView adicionado

--- app/src/main/java/com/campones/listadetarefas/FirstFragment.java
+++ app/src/main/java/com/campones/listadetarefas/FirstFragment.java
@@ -1,19 +1,36 @@
 package com.campones.listadetarefas;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.campones.listadetarefas.adapter.TarefaAdapter;
 import com.campones.listadetarefas.databinding.FragmentFirstBinding;
+import com.campones.listadetarefas.helper.RecyclerItemClickListener;
+import com.campones.listadetarefas.model.Tarefa;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class FirstFragment extends Fragment {
 
     private FragmentFirstBinding binding;
+
+    private RecyclerView recyclerView;
+    private TarefaAdapter tarefaAdapter;
+    private List<Tarefa> listaTarefas = new ArrayList<>();
 
     @Override
     public View onCreateView(
@@ -22,20 +39,59 @@ public class FirstFragment extends Fragment {
     ) {
 
         binding = FragmentFirstBinding.inflate(inflater, container, false);
-        return binding.getRoot();
+        //recyclerView
+        recyclerView = binding.recyclerView;
 
+        recyclerView.addOnItemTouchListener(
+                new RecyclerItemClickListener(
+                        getActivity(),
+                        recyclerView,
+                        new RecyclerItemClickListener.OnItemClickListener() {
+                            @Override
+                            public void onItemClick(View view, int position) {
+                                Log.i("clique", "onItemClick");
+                            }
+
+                            @Override
+                            public void onLongItemClick(View view, int position) {
+                                Log.i("clique", "onLongItemClick");
+                            }
+
+                            @Override
+                            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                            }
+                        }
+                )
+        );
+        return binding.getRoot();
     }
 
-    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
+    public void carregarListaTarefas(){
+        //Listar tarefas
+        Tarefa tarefa1 = new Tarefa();
+        tarefa1.setNomeTarefa("Ir ao mercado");
+        listaTarefas.add( tarefa1 );
 
-        binding.buttonFirst.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                NavHostFragment.findNavController(FirstFragment.this)
-                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
-            }
-        });
+        Tarefa tarefa2 = new Tarefa();
+        tarefa2.setNomeTarefa("Ir ao feira");
+        listaTarefas.add( tarefa2 );
+
+        //Configurar um adapter
+        tarefaAdapter = new TarefaAdapter( listaTarefas );
+
+        //Configurar RecyclerView
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager( getActivity() );
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.addItemDecoration(new DividerItemDecoration(getActivity(), LinearLayout.VERTICAL));
+        recyclerView.setAdapter(tarefaAdapter);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        carregarListaTarefas();
     }
 
     @Override
