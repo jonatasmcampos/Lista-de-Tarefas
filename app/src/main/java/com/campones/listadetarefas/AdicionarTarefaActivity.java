7a6fb0b071403932ccd5ce551810e3ad19162b05
jonatasmcampos <camposjonatas3@gmail.com>
2022-07-18 20:44:47 -0300

    menu adicionado

--- app/src/main/java/com/campones/listadetarefas/AdicionarTarefaActivity.java
+++ app/src/main/java/com/campones/listadetarefas/AdicionarTarefaActivity.java
@@ -1,8 +1,12 @@
 package com.campones.listadetarefas;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class AdicionarTarefaActivity extends AppCompatActivity {
 
@@ -10,5 +14,24 @@ public class AdicionarTarefaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adicionar_tarefa);
+    }
+
+    //Configurando o menu
+    @Override
+    public boolean onCreateOptionsMenu(@NonNull Menu menu) {
+        getMenuInflater().inflate( R.menu.menu_adicionar_tarefa, menu );
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    //Verificando o click no item do menu
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.itemSalvar:
+                //Executa ação para salvar
+                Toast.makeText(this, "Botao salvar pressionado", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
